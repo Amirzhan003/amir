874597a3b49d486d28a6b75ffc3f8f30083fcd1c
Amirzhan003 <163534371+Amirzhan003@users.noreply.github.com>
2025-04-10 16:15:03 +0500

    API2.py

---
+++ API2.py
@@ -0,0 +1,22 @@
+Microsoft Visual Studio Solution File, Format Version 12.00
+ # Visual Studio Version 17
+ VisualStudioVersion = 17.7.34031.279
+ MinimumVisualStudioVersion = 10.0.40219.1
+ Project("{888888A0-9F3D-457C-B088-3A5042F75D52}") = "API", "API.pyproj", "{6A157A71-A43E-442E-8E69-9515C157754C}"
+ EndProject
+ Global
+ 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+ 		Debug|Any CPU = Debug|Any CPU
+ 		Release|Any CPU = Release|Any CPU
+ 	EndGlobalSection
+ 	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+ 		{6A157A71-A43E-442E-8E69-9515C157754C}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+ 		{6A157A71-A43E-442E-8E69-9515C157754C}.Release|Any CPU.ActiveCfg = Release|Any CPU
+ 	EndGlobalSection
+ 	GlobalSection(SolutionProperties) = preSolution
+ 		HideSolutionNode = FALSE
+ 	EndGlobalSection
+ 	GlobalSection(ExtensibilityGlobals) = postSolution
+ 		SolutionGuid = {B3D899A0-9327-4052-AA7F-78A148B54BE7}
+ 	EndGlobalSection
+ EndGlobal
